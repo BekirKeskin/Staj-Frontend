dbdcd726c4b80447e6d09166b99951916881df83
BekirKeskin <keskinbekir2834@gmail.com>
2026-07-09 17:23:16 +0300

    reactive form validation tamamlandı

--- angular-deneme/src/app/features/todos/todo-form/todo-form.ts
+++ angular-deneme/src/app/features/todos/todo-form/todo-form.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 
 @Component({
@@ -11,20 +11,48 @@ import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angula
 })
 
 export class TodoForm implements OnInit{
+  categories = ["Work", "Study", "Personal"]
 
   @Output()
   todoAdded = new EventEmitter<{ title: string; description: string }>();
 
-  todoForm = new FormGroup({
+  todoForm = new FormGroup({ // !!!!!!!!!! Reactive forms un başladığı yer !!!!!!!!!!
     title: new FormControl('',[ // form control = tek bir input
-      Validators.required, // validators kurallarmış boş olamaz 3 karakterden kısa olamaz.
+      Validators.required, // validators kurallarmış, boş olamaz 3 karakterden kısa olamaz.
       Validators.minLength(3)
     ]),
     description: new FormControl('',[
       Validators.required,
       Validators.minLength(1)
+    ]),
+    priority: new FormControl('',[
+      Validators.required
+    ]),
+    dueDate: new FormControl('',[
+      Validators.required,
+      this.noPastDateValidator
+    ]),
+    category: new FormControl('',[
+      Validators.required,
     ])
   });
+
+  noPastDateValidator(control: AbstractControl):ValidationErrors | null{
+    const date = control.value;
+    
+    if(!date){
+      return null;
+    }
+    const selectedDate = new Date(date);
+    const today = new Date();
+    selectedDate.setHours(0,0,0,0);
+    today.setHours(0,0,0,0);
+
+    if(selectedDate < today){
+      return{ pastDate:true};
+    }
+    return null;
+  }
 
   onSubmit(){
     
